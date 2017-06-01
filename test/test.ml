75e0645d9c1b8824c92d4b8e5a4fd93df148cc13
Tianlu Chen <tchen2@swarthmore.edu>
2017-06-01 16:41:13 -0400

    config

--- test/test.ml
+++ test/test.ml
@@ -3,8 +3,10 @@ open OUnit2;;
 let all_tests =
   "tests" >:::
   [
-    Test_parser.tests;
-    Test_abstract_ast.tests;
+    (* Test_parser.tests; *)
+    (* Test_abstract_ast.tests; *)
+    (* Test_intentional_failures.tests; *)
+    Test_rename.tests
   ]
 ;;
 
