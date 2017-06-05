7c3400f61824988746bc65ac3bdafe9f4a390a29
Devon Loehr <dloehr1@swarthmore.edu>
2017-06-05 13:06:08 -0400

    Add some super-basic cfg rules. Very soon we should give them their own file

--- src/analysis/python2_cfg.ml
+++ src/analysis/python2_cfg.ml
@@ -33,25 +33,54 @@ module Control_cfg = Graph.Make(Control_vertex);;
 (* TODO: Add a signature and, y'know, the rest of the functionality *)
 module Cfg =
 struct
-  type t = Cfg of Lexical_cfg.t * Control_cfg.t;;
+  type t = Cfg of Lexical_cfg.t * Control_cfg.t;; (* TODO: Make this a record? *)
 
   let add_control_edge (e : Control_cfg.edge) (curr : t) : t =
     begin
       match curr with
       | Cfg (lex, ctrl) -> Cfg (lex, Control_cfg.add_edge e ctrl)
     end
+  ;;
+
+  let active_vertex _ _ = true;; (* TODO *)
+  let immediate_vertex _ = true;; (* TODO *)
+
+  (* TODO: Move rules to another file, probably *)
+  let start_rule (_ : t) (e : Lexical_cfg.edge) : Control_cfg.edge list =
+    match e with
+    | Lexical_cfg.Edge (Start, nxt) -> [Control_cfg.Edge(Start, nxt)]
+    | _ -> []
+  ;;
+
+  let immediate_rule (curr : t) (e : Lexical_cfg.edge) : Control_cfg.edge list =
+    match e with
+    | Lexical_cfg.Edge (v1, v2) ->
+      if (active_vertex v1 curr) && (immediate_vertex v1)
+      then
+        [Control_cfg.Edge(v1,v2)]
+      else
+        []
+  ;;
+
+  let apply_rules (curr : t) (e : Lexical_cfg.edge) : Control_cfg.edge list =
+    start_rule curr e @ immediate_rule curr e;;
 
   let get_edges_to_add (curr : t) : Control_cfg.edge list =
     match curr with
     | Cfg (lex, ctrl) -> ignore ctrl; (* FIXME *)
       let rec collect_edges_from_vertex (v : vertex) : Control_cfg.edge list =
         let outgoing_edges = Lexical_cfg.edges_from v lex in
-        ignore outgoing_edges; (* Call edge rules on each *)
+        let edges_to_add =
+          Enum.fold (fun l1 l2 -> l1 @ l2) (* TODO: This makes me sad *)
+            []
+            (Enum.map (apply_rules curr) outgoing_edges) in
         let successors = Lexical_cfg.succs v lex in
+        (* If there are cycles in the lexical cfg this will never end *)
         Enum.fold
-          (fun lst v -> lst @ collect_edges_from_vertex v) [] successors
+          (fun lst v -> lst @ collect_edges_from_vertex v) edges_to_add successors
       in (* End definition of collect_edges_from_vertex *)
       collect_edges_from_vertex Start
+  ;;
 
   (* TODO: Deal with stuff like if statements that have subordinate stmt lists *)
   let create_lexical_cfg (m : modl) =
@@ -76,6 +105,6 @@ struct
 
   let create (m : modl) : t =
     Cfg(create_lexical_cfg m, Control_cfg.empty)
-
+  ;;
 
 end;;
