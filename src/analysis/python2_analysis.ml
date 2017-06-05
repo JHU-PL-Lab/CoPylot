c6fa8092878a207fe45b5fea0bd22bab3a99d5b7
Devon Loehr <dloehr1@swarthmore.edu>
2017-06-05 12:48:45 -0400

    Start implementing the cfg and pds building

--- src/analysis/python2_analysis.ml
+++ src/analysis/python2_analysis.ml
@@ -7,16 +7,33 @@ module Analysis_result =
 struct
   type t =
     {
-      analysis_cfg: cfg;
+      analysis_cfg: Cfg.t;
       analysis_pds: pds;
     }
 
-  let create (prog : modl) : t = ignore prog; raise Utils.Not_yet_implemented;;
+  let add_edge (curr : t) (e : Control_cfg.edge) : t =
+    let new_cfg = Cfg.add_control_edge e curr.analysis_cfg in
+    let new_pds = curr.analysis_pds in (* TODO: Edge functions *)
+    { analysis_cfg = new_cfg; analysis_pds = new_pds }
 
   let query
       (analysis : t) (prog_point : uid) (var : identifier) : Answer_set.t =
-    ignore analysis; ignore prog_point; ignore var;
+    (* Call closure *) ignore analysis; ignore prog_point; ignore var;
     raise Utils.Not_yet_implemented;;
+
+  let rec build_cfg_and_pds (curr : t) : t =
+    let edges_to_add = Cfg.get_edges_to_add curr.analysis_cfg in
+    match edges_to_add with
+    | [] -> curr
+    | _ ->
+      let result = List.fold_left add_edge curr edges_to_add in
+      build_cfg_and_pds result
+
+  let create (prog : modl) : t =
+    let cfg = Cfg.create prog in
+    let pds = Python2_pds.Reachability.empty () in
+    build_cfg_and_pds ({ analysis_cfg = cfg; analysis_pds = pds })
+
 end;;
 
 let analyze (prog : modl) (prog_point : uid) (var : identifier) : Answer_set.t =
--- src/analysis/python2_cfg.ml
+++ src/analysis/python2_cfg.ml
@@ -1,34 +1,81 @@
 open Batteries;;
 open Python2_normalized_ast;;
 
-type cfg_lexical_edge =
-  | Cfg_lexical_edge of stmt * stmt
+type vertex =
+  | Prog_point of stmt
+  | Start
+  | End
 [@@deriving eq, ord, show]
 ;;
 
-type cfg_control_edge =
-  | Cfg_control_edge of stmt * stmt
-[@@deriving eq, ord, show]
+module Lexical_vertex =
+struct
+  type t = vertex
+  let compare = compare_vertex
+  let pp = pp_vertex
+  let equal = equal_vertex
+end
 ;;
 
-module Cfg_lexical_edge =
+module Control_vertex =
 struct
-  type t = cfg_lexical_edge
-  let compare = compare_cfg_lexical_edge
-  let pp = pp_cfg_lexical_edge
-  let equal = equal_cfg_lexical_edge
-end;;
+  type t = vertex
+  let compare = compare_vertex
+  let pp = pp_vertex
+  let equal = equal_vertex
+end
+;;
 
-module Cfg_control_edge =
+module Lexical_cfg = Graph.Make(Lexical_vertex);;
+
+module Control_cfg = Graph.Make(Control_vertex);;
+
+(* TODO: Add a signature and, y'know, the rest of the functionality *)
+module Cfg =
 struct
-  type t = cfg_control_edge
-  let compare = compare_cfg_control_edge
-  let pp = pp_cfg_control_edge
-  let equal = equal_cfg_control_edge
+  type t = Cfg of Lexical_cfg.t * Control_cfg.t;;
+
+  let add_control_edge (e : Control_cfg.edge) (curr : t) : t =
+    begin
+      match curr with
+      | Cfg (lex, ctrl) -> Cfg (lex, Control_cfg.add_edge e ctrl)
+    end
+
+  let get_edges_to_add (curr : t) : Control_cfg.edge list =
+    match curr with
+    | Cfg (lex, ctrl) -> ignore ctrl; (* FIXME *)
+      let rec collect_edges_from_vertex (v : vertex) : Control_cfg.edge list =
+        let outgoing_edges = Lexical_cfg.edges_from v lex in
+        ignore outgoing_edges; (* Call edge rules on each *)
+        let successors = Lexical_cfg.succs v lex in
+        Enum.fold
+          (fun lst v -> lst @ collect_edges_from_vertex v) [] successors
+      in (* End definition of collect_edges_from_vertex *)
+      collect_edges_from_vertex Start
+
+  (* TODO: Deal with stuff like if statements that have subordinate stmt lists *)
+  let create_lexical_cfg (m : modl) =
+    let stmt_list_to_vertex_list (lst : stmt list) : vertex list =
+      List.map (fun stmt -> Prog_point(stmt)) lst
+    in
+    let vertex_list_to_edge_list (lst : vertex list) : Lexical_cfg.edge list =
+      List.map2
+        (fun v1 v2 -> Lexical_cfg.Edge(v1, v2))
+        ([Start] @ lst)
+        (lst @ [End])
+    in
+    match m with
+    | Module (stmts, _) ->
+      let v_lst = stmt_list_to_vertex_list stmts in
+      let e_lst = vertex_list_to_edge_list v_lst in
+      List.fold_left
+        (fun gph e -> Lexical_cfg.add_edge e gph)
+        Lexical_cfg.empty
+        e_lst
+  ;;
+
+  let create (m : modl) : t =
+    Cfg(create_lexical_cfg m, Control_cfg.empty)
+
+
 end;;
-
-module Lexical_cfg = Graph.Make(Cfg_lexical_edge);;
-
-module Control_cfg = Graph.Make(Cfg_control_edge);;
-
-type cfg = Cfg of Lexical_cfg.t * Control_cfg.t;;
